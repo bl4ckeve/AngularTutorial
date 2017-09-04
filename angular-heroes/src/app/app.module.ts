374c29b82423150bcb8a394e662130c0f3fd79d7
bl4ckeve <1664268+bl4ckeve@users.noreply.github.com>
2017-09-04 11:45:10 +0200

    Step 6 - refactor routing -> AppRoutingModule

--- angular-heroes/src/app/app.module.ts
+++ angular-heroes/src/app/app.module.ts
@@ -6,33 +6,15 @@ import { AppComponent }  from './app.component';
 import {HeroDetailComponent} from './hero-detail.component';
 import {HeroesComponent} from './heroes.component';
 import {HeroService} from './hero.service';
-import {RouterModule} from '@angular/router';
 import {MenuComponent} from './menu.component';
 import {DashboardComponent} from './dashboard.component';
+import {AppRoutingModule} from './app-routing.module';
 
 @NgModule({
   imports: [
     BrowserModule,
     FormsModule,
-    RouterModule.forRoot([
-      {
-        path: 'heroes',
-        component: HeroesComponent
-      },
-      {
-        path: 'dashboard',
-        component: DashboardComponent
-      },
-      {
-        path: '',
-        redirectTo: 'dashboard',
-        pathMatch: 'full'
-      },
-      {
-        path: 'heroes/detail/:id',
-        component: HeroDetailComponent
-      }
-    ])
+    AppRoutingModule,
   ],
   declarations: [
     AppComponent,
